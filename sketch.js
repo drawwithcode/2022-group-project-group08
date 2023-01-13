16a82114cf2935c3a782ef7ba299a77d5c694209
scardinanoire <andre.corsi@outlook.it>
2023-01-13 15:10:06 +0100

    1

--- sketch.js
+++ sketch.js
@@ -1,16 +1,45 @@
-function preload() {
-	// put preload code here
-}
+let fallimg = [];
+let posimg = [];
 
-function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
-}
+let fallshape = [];
+let posshape = [];
 
-function draw() {
-	// put drawing code here
-}
+let nimg;
+let nshape;
+
+let timg;
+let tshape;
+
+let cnv1;
+
+let rain = function (r) {
+  r.preload = function () {
+    cnv1 = r.createCanvas(r.windowWidth, r.windowHeight);
+    nimg = r.round(r.random(0, 15));
+    nshape = r.round(r.random(0, 15));
+
+    console.log(fallimg[0]);
+
+    for (let i = 0; i < nimg; i += 2) {
+      timg = r.round(r.random(0, fallimg.lenght));
+      posimg[i] = r.random(0, r.width);
+      posimg[i + 1] = 50;
+      r.image(fallimg[timg], 0, 0, 100, 100);
+    }
+  };
+
+  r.setup = function () {
+    for (let i = 0; i < 1; i++) {
+      fallimg[i] = r.loadImage("Assets/Photos/i" + i + ".png");
+      console.log(i);
+    }
+
+    for (let i = 0; i < 1; i++) {
+      fallshape[i] = r.loadImage("Assets/Photos/s" + i + ".png");
+    }
+  };
+
+  r.draw = function () {};
+};
+
+let rainingobjects = new p5(rain);
