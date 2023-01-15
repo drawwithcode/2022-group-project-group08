a0561648d752ea912d43940a4cf2958c24102dd5
scardinanoire <andre.corsi@outlook.it>
2023-01-15 11:45:44 +0100

    prova

--- sketch.js
+++ sketch.js
@@ -1,5 +1,3 @@
-//-----------------------------------INITIAL ANIMATION----------------------------
-
 let fallimg = [];
 let posximg = [];
 let posyimg = [];
@@ -9,67 +7,56 @@ let imgangle = [];
 
 let nimg;
 
-let imgtype = 6;
+let imgtype = 16;
 
 let timg = [];
 
 let cnv1;
 
-let rain = function (r) {
-  r.preload = function () {
-    for (let i = 0; i <= imgtype; i++) {
-      fallimg[i] = r.loadImage("Assets/Photos/i" + i + ".png");
+function preload() {
+  for (let i = 0; i <= imgtype; i++) {
+    fallimg[i] = loadImage("Assets/Photos/i" + i + ".png");
+  }
+}
+
+function setup() {
+  cnv1 = createCanvas(windowWidth, windowHeight);
+
+  nimg = round(random(10, 15));
+  angleMode(DEGREES);
+
+  for (let i = 0; i < nimg; i++) {
+    timg[i] = round(random(0, imgtype));
+    posximg[i] = random(0, width);
+    posyimg[i] = random(0, -height);
+    velimgy[i] = random(1, 5);
+    imgsize[i] = random(100, 200);
+    imgangle[i] = random(0, 360);
+  }
+}
+
+function draw() {
+  background("#1e1e1e");
+
+  for (let i = 0; i < nimg; i++) {
+    push();
+    translate(posximg[i], posyimg[i]);
+    rotate(imgangle[i]);
+
+    image(fallimg[timg[i]], 0, 0, imgsize[i], imgsize[i]);
+    posyimg[i] += velimgy[i];
+
+    if (posyimg[i] > height + 200) {
+      posyimg[i] = random(0, -height);
+      posximg[i] = random(0, width);
+      imgsize[i] = random(100, 200);
+      velimgy[i] = random(1, 5);
+      imgangle[i] = random(0, 360);
     }
-  };
+    pop();
+  }
+}
 
-  r.setup = function () {
-    cnv1 = r.createCanvas(r.windowWidth, r.windowHeight);
-    cnv1.parent("rainingmen");
-
-    nimg = r.round(r.random(10, 15));
-    r.angleMode(r.DEGREES);
-
-    for (let i = 0; i < nimg; i++) {
-      timg[i] = r.round(r.random(0, imgtype));
-      posximg[i] = r.random(0, r.width);
-      posyimg[i] = r.random(0, -r.height);
-      velimgy[i] = r.random(1, 5);
-      imgsize[i] = r.random(100, 200);
-      imgangle[i] = r.random(0, 360);
-    }
-  };
-
-  r.draw = function () {
-    r.background("#1e1e1e");
-
-    for (let i = 0; i < nimg; i++) {
-      r.push();
-      r.translate(posximg[i], posyimg[i]);
-      r.rotate(imgangle[i]);
-
-      r.image(fallimg[timg[i]], 0, 0, imgsize[i], imgsize[i]);
-      posyimg[i] += velimgy[i];
-
-      if (posyimg[i] > r.height + 200) {
-        posyimg[i] = r.random(0, -r.height);
-        posximg[i] = r.random(0, r.width);
-        imgsize[i] = r.random(100, 200);
-        velimgy[i] = r.random(1, 5);
-        imgangle[i] = r.random(0, 360);
-      }
-      r.pop();
-    }
-  };
-
-  r.windowResized = function () {
-    r.resizeCanvas(r.windowWidth, r.windowHeight);
-  };
-};
-
-let rainingobjects = new p5(rain);
-
-//--------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------
-//-----------------------------------INITIAL ANIMATION----------------------------
-//--------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
